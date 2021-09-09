eb62da0ed67f2f0e713a1b83f81a75653786b712
Miranda0428 <elowey81@gmail.com>
2021-09-09 14:47:06 +0900

    Update p96_3-3.py

--- This_is_coding_test/p96_3-3.py
+++ This_is_coding_test/p96_3-3.py
@@ -1,1 +1,24 @@
-print("new")
+import time
+
+def test():
+
+    #n, m, k = map(int, input().split())
+    #data = list(map(int, input().split()))
+    n, m = 2, 4
+    data = [[7, 3, 1, 8], [3, 3, 3, 4]]
+
+    result = 0
+
+    for i in range(n):
+        if result < min(data[i]):
+            result = min(data[i])
+
+    return result
+
+if __name__ == '__main__':.
+
+    start_time = time.time()
+    print(test())
+    end_time = time.time()
+
+    print("time : ", end_time - start_time)
