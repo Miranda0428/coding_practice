3a547313dbc297db1c3315b6c8262964430d5eea
Miranda0428 <elowey81@gmail.com>
2021-10-08 17:04:26 +0900

    Add files via upload

---
+++ This_is_coding_test/p359_Q23.py
@@ -0,0 +1,28 @@
+# def setting_1(tuple):
+#     return tuple[1]
+#
+# def setting_2(tuple):
+#     return tuple[2]
+#
+# def setting_3(tuple):
+#     return tuple[3]
+#
+# def setting_4(tuple):
+#     return tuple[0]
+
+def solution():
+
+    n = int(input())
+    data = [0] * n
+
+    for i in range(n):
+        data[i] = tuple(input().split())
+
+    data.sort(key=lambda x: (-int(x[1]),int(x[2]),-int(x[3]),x[0]))  # sort에서의 lambda 사용 외우기!!
+                                                                     # map(int, input().split())은 띄어쓰기로 구분되는 input에 대해서 모두 int 함수를 적용한다는 뜻
+
+    for i in range(n):
+        print(data[i][0], end=' ')
+
+if __name__=="__main__":
+    solution()
---
+++ This_is_coding_test/p367_Q27.py
@@ -0,0 +1,32 @@
+def solution():
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    start = 0
+    end = n-1
+
+    while True:
+        if start > end:
+            return -1
+        mid = (start+end)//2
+        if m == data[mid]:
+            break
+        elif m < data[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    i = mid
+    count = 0
+    while data[i] == data[i-1]:
+        i-=1
+        count+=1
+
+    i = mid
+    while data[i] == data[i+1]:
+        i+=1
+        count +=1
+    return count+1
+
+if __name__ == "__main__":
+    print(solution())
