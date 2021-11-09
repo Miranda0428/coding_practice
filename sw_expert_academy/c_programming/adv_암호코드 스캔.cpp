d7fb1810c01c364b06ca69d29fa6f49d74faaf8d
Miranda0428 <elowey81@gmail.com>
2021-11-09 17:05:34 +0900

    Add files via upload

--- sw_expert_academy/c_programming/adv_암호코드 스캔.cpp
+++ sw_expert_academy/c_programming/adv_암호코드 스캔.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 
+#pragma warning(disable:4996)
+
 using namespace std;
 
 int Hashtable[7000];
@@ -24,6 +26,7 @@ int main(int argc, char **argv) {
 	Hashtable[3112] = 9;
 
 	int tcCnt;
+	freopen("sample_input.txt", "r", stdin);
 	cin >> tcCnt;
 	for (int t = 0; t < tcCnt; ++t) {
 		cin >> row >> col;
@@ -33,7 +36,6 @@ int main(int argc, char **argv) {
 		
 		answer[t] = solve();
 	}
-	cout << endl;
 	for (int t = 0; t < tcCnt; ++t)
 		cout << "#" << t + 1 << ' ' << answer[t] << endl;
 	return 0;
@@ -41,21 +43,26 @@ int main(int argc, char **argv) {
 /* 2차원 배열 arr[100][100]을 함수 인자로 전달하기 위해서는 char arr[][100] 또는
    char (*arr)[100] 으로 전달해야 한다. */
 void hex_to_binarr(char (*arr)[2000]) {
-	char hex_value[16][5] = { "0000", "0001", "0010", "0011", "0100", "0101", "0111",
-		"1000", "1001", "1010", "1011", "1100", "1101", "1111" };
-	char tmp[2000];
+	char hex_value[16][5] = { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
+		"1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" };
+	char* tmp = new char[col+1];
 	char a = 0;
 	for (int i = 0; i < row; ++i) {
-		copy(arr[i], arr[i]+col, tmp);
+		for (int t = 0; t < col; ++t)
+			tmp[t] = arr[i][t];
+		tmp[col] = '\0';
 		for (int j = 0; j < col; ++j) {
 			if (tmp[j] >= 'A')                 //16 진수를 10진수를 바꿔서 hex_value의 인덱스로 사용한다.
 				a = tmp[j] - 'A' + 10;
 			else
 				a = tmp[j] - '0';
-			for (int k = 0; k < 4; ++k)
+			for (int k = 0; k < 4; ++k) {
 				arr[i][j * 4 + k] = hex_value[a][k];
+			}
 		}
 	}
+	col = col * 4;
+	delete[] tmp;
 }
 
 int calscale(char *arr, int j) {
@@ -69,11 +76,10 @@ int calscale(char *arr, int j) {
 	}
 }
 
-int* parse(int i, int start, int scale) {
-	int code[8] = { 0, };
+int parse(int* code, int i, int start, int scale) {
 	int idx = 0;
 	int scale_times = 7 * scale;
-	for (int k = start; ; k += scale_times) {
+	for (int k = start; idx < 8; k += scale_times) {
 		int key = 0;
 		int cnt = 1;
 		for (int a = 0; a < scale_times; ++a) {
@@ -87,12 +93,12 @@ int* parse(int i, int start, int scale) {
 		}
 		code[idx++] = Hashtable[key];
 	}
-	return code;
+	return 0;
 }
 
 int solve() {
 	int* code;
-	int code_arr[8];
+	int code_arr[8] = {0,};
 	int flag = 0; 
 	int sum = 0;
 	int scale = 0;
@@ -102,12 +108,13 @@ int solve() {
 	hex_to_binarr(arr);
 
 	for (int i = 0; i < row; ++i) {
+		//i = 10;
 		for (int j = col - 1; j >= 55; --j) {
 			if (arr[i][j] == '1') {
 				flag = true;
 				scale = calscale(arr[i], j);
 				start_col = j - 56 * scale + 1;
-				code = parse(i, start_col, scale);   // hashtable을 리턴하는 함수
+				parse(code, i, start_col, scale);   // hashtable을 리턴하는 함수
 				for (int k=0; k < 8; ++k)
 					if (code[k] < 0){
 						flag = false;
@@ -129,8 +136,8 @@ int solve() {
 							sum += code[i];
 					}
 				}
+				j = start_col - 1;
 			}
-			j = start_col - 1;
 		}
 	}
 	return sum;
