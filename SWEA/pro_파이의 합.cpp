ef5c87e826d772df70138a7cef880c6c34389ff6
Miranda0428 <elowey81@gmail.com>
2022-01-01 15:58:56 +0900

    Add files via upload

--- SWEA/pro_파이의 합.cpp
+++ SWEA/pro_파이의 합.cpp
@@ -1,4 +1,4 @@
-/* 에라토스테네스의 체 */
+/* 같은 코드인데 JAVA 코드는 Pass이고 C++ 코드는 시간초과란다. */
 #include <iostream>
 #include <algorithm>
 
@@ -11,31 +11,33 @@ int tcCnt;
 int start, endv;
 int arr[MAX_N];
 int ps[MAX_N];
-bool prime[MAX_N];
+bool comp[MAX_N];
 
 int solve() {
-    int sqroot = sqrt(endv);
-    for (int i = 2; i <= sqroot; i++) {
-        for (int j = 2; j <= sqroot; j++) {
-            prime[i * j] = true;
-        }
-    }
-
-    for (int a = 0; a <= endv; a++) {
-        arr[a] = a - 1;
-    }
-
-    for (int i = 2; i <= endv; i++) {
-        if (!prime[i]) {
-            for (int j = 1; i*j <= endv; j++) {
-                arr[i * j] = (i - 1) * arr[j];
-                //arr[i * j] = (i - 1) * (arr[i * j]/i);
+    comp[1] = true;
+    for (int i = 2; i*i <= endv; i++) {
+        if (comp[i] == false) {
+            for (int j = 2; i * j <= endv; j++) {
+                comp[i * j] = true;
             }
         }        
     }
 
-    ps[0] = 0;
     for (int i = 1; i <= endv; i++)
+        arr[i] = i;
+
+    for (int i = 2; i <= endv; i++) {
+        // 소수이면
+        if (comp[i]==false) {
+            // 배수들에 대한 처리
+            for (int j = 1; i * j <= endv; j++) {
+                arr[i * j] = (i - 1) * (arr[i * j] / i);
+            }
+        }
+    }
+
+    ps[0] = 0; ps[1] = 1;
+    for (int i = 2; i <= endv; i++)
         ps[i] = ps[i - 1] + arr[i];
 
     return ps[endv] - ps[start-1];
@@ -45,7 +47,7 @@ int main() {
     freopen("input.txt", "r", stdin);
     cin >> tcCnt;
     for (int t = 0; t < tcCnt; ++t) {
-        memset(arr, 0x00, sizeof(int) * MAX_N);
+        //memset(arr, 0x00, sizeof(int) * MAX_N);
         cin >> start >> endv;
         cout << "#" << t + 1 << ' ' << solve() << endl;
     }
