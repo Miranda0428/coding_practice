9ebb51140da029e1a71daeeb3904c0b4659e046e
Miranda0428 <elowey81@gmail.com>
2022-06-22 16:00:32 +0900

    Add files via upload

---
+++ SWEA/pro_그래도 수명이 절반이 되어서는.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+#define MAX_W 200000
+
+int tcCnt;
+int N, K;
+int W[MAX_W+1];
+int S[MAX_W+1];
+
+bool IsPossible(int middle) {
+    int cnt = 0;
+    int data = 0;
+
+    for (int i = 0; i < MAX_W; ++i) {
+        if (W[i] <= middle) cnt++;
+        else cnt = 0;
+
+        if (cnt == S[data]) {
+            data++;
+            cnt = 0;
+        }
+
+        if (data == K)
+            return true;
+    }
+
+    return false;
+}
+
+int solve(int start, int end) {
+
+    while (start < end) {
+        int mid = (start + end) / 2;
+
+        if (IsPossible(mid))
+            end = mid;
+        else
+            start = mid + 1;
+    }
+
+    return end;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    cin >> tcCnt;
+    for (int t = 0; t < tcCnt; ++t) {
+        cin >> N;
+        cin >> K;
+        for (int i = 0; i < N; ++i) {
+            scanf("%d", &W[i]);
+        }
+        for (int i = 0; i < K; ++i) {
+            scanf("%d", &S[i]);
+        }
+        cout << "#" << t + 1 << ' ';
+        cout << solve(1, MAX_W);
+        cout << endl;
+    }
+    return 0;
+}
